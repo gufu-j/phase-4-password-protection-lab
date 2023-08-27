d9d11202ba46f46ceb9e07d1280cd4c2947e5560
diego <guatemaladiegojose@gmail.com>
2023-08-26 21:30:53 -0400

    created user and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
 end
