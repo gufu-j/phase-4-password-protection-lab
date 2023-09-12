c810e9afc39f5cb5fbba133858dfe5ab7b75deca
diego <guatemaladiegojose@gmail.com>
2023-09-11 22:56:34 -0400

    corrected user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  before_action :authorize, only: [:show]
+
+
+
     def create
         user = User.create(user_params)
         if user.valid?
@@ -11,17 +15,32 @@ class UsersController < ApplicationController
     end
     
 
-    def show 
-        user = User.find_by(id: session[:user_id])
-            if user
-                render json: user
-             else
-                render json: { error: "Not authorized" }, status: :unauthorized
-        end
-    end 
+    # def show 
+    #     user = User.find_by(id: session[:user_id])
+    #         if user
+    #             render json: user
+    #          else
+    #             render json: { error: "Not authorized" }, status: :unauthorized
+    #     end
+    # end 
 
+    # private
+
+    # def user_params
+    #   params.permit(:username, :password, :password_confirmation)
+    # end
+
+    def show
+      user = User.find_by(id: session[:user_id])
+      render json: user
+    end
+  
     private
-
+  
+    def authorize
+      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
+  
     def user_params
       params.permit(:username, :password, :password_confirmation)
     end
